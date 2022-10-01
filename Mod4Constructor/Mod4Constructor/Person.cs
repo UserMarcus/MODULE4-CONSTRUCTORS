d7507a29dd3b3a53d965f0c9bf25c22545bc77bf
UserMarcus <114788534+UserMarcus@users.noreply.github.com>
2022-10-01 10:51:44 +0800

    Add files via upload

---
+++ Mod4Constructor/Mod4Constructor/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace Mod4Constructor
+{
+    class Person
+    {
+
+        public string Firstname;
+        public string Lastname;
+
+        public Person(string Firstname, string Lastname)
+        {
+            this.Firstname = Firstname;
+            this.Lastname = Lastname;
+        }
+
+        public int BirthYear;
+
+        public Person(int BirthYear)
+        {
+            this.BirthYear = BirthYear;
+        }
+
+        public void GetFullName()
+        {
+            System.Action Person = GetFullName;
+        }
+
+        public void GetAge()
+        {
+            System.Action BirthYear = GetAge;
+        }
+
+    }
+}
---
+++ Mod4Constructor/Mod4Constructor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod4Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person1 = new Person("Angel" , "Locsin");
+
+            person1.BirthYear = 30;
+
+            Console.WriteLine("Hello" + person1);
+            Console.WriteLine("You are " + person1.BirthYear + "years old");
+            Console.ReadKey();
+        }
+    }
+}
